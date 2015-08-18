0c9cb243732218dfc872a0308c4b42fc04f08bb2
sirMackk <c.tairix@gmail.com>
2015-08-18 06:09:03 +0200

    Updates csv2md.py

--- scripts/csv2md.py
+++ scripts/csv2md.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import fileinput
 import sys
 import csv
@@ -7,7 +9,7 @@ def process_line(csv_line):
     # replace <!!!> with newline - done
     # format date
     headers = ['no', 'mys1', 'title', 'desc', 'slug', 'contents', 'created', 'updated', 'tra1', 'tra2']
-    reader = csv.reader(csv_line, quoting=csv.QUOTE_ALL, quotechar='|')
+    reader = csv.reader(csv_line, quoting=csv.QUOTE_ALL, quotechar="\t")
     row = [r[0] for r in reader if r and r != ['', '']]
 
     entry = dict(zip(headers, row))
