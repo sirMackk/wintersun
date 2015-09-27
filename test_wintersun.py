89f13ec514496e5c2d35fb29b65f8d69d7571655
sirMackk <c.tairix@gmail.com>
2015-09-27 19:09:00 +0200

    Adds tests before First Refactor

--- test_wintersun.py
+++ test_wintersun.py
@@ -16,12 +16,12 @@ class TestWintersun(unittest.TestCase):
 
     @mock.patch('wintersun.os_path')
     @mock.patch('wintersun.os')
-    def test_get_items_from_path_dirs(self, mock_os, mock_os_path):
+    def test_filter_items_from_path_dirs(self, mock_os, mock_os_path):
         mock_os.listdir.return_value = ['file1', 'dir1']
         mock_os_path.isdir.side_effect = lambda full_path: full_path == 'dir1'
         mock_os_path.join.side_effect = lambda path, filename: filename
 
-        dirs = list(wintersun.get_items_from_path(
+        dirs = list(wintersun.filter_items_from_path(
             './current-dir',
             mock_os_path.isdir))
 
@@ -32,18 +32,245 @@ class TestWintersun(unittest.TestCase):
 
     @mock.patch('wintersun.os_path')
     @mock.patch('wintersun.os')
-    def test_get_items_from_path_files(self, mock_os, mock_os_path):
+    def test_filter_items_from_path_files(self, mock_os, mock_os_path):
         mock_os.listdir.return_value = ['file1', 'dir1']
         mock_os_path.isfile.side_effect = lambda x: x == 'file1'
         mock_os_path.join.side_effect = lambda x, y: y
 
-        files = list(wintersun.get_items_from_path(
+        files = list(wintersun.filter_items_from_path(
             './current-dir',
             mock_os_path.isfile))
 
         self.assertEqual(mock_os_path.join.call_count, 2)
         mock_os.listdir.assert_called_with('./current-dir')
         self.assertEqual(files, ['file1'])
+
+    def test_standardize_filename_with_leading_slashes(self):
+        filepath = '/home/test/files/markdown.md'
+        self.assertEqual(
+            wintersun.standardize_filename(filepath), 'markdown')
+
+    def test_standardize_filename_with_relative_slashes(self):
+        filepath = './tests/markdown.md'
+        self.assertEqual(
+            wintersun.standardize_filename(filepath), 'markdown')
+
+    def test_standardize_filename_with_trailing_suffix(self):
+        filepath = '/home/test/files/markdown.md'
+        self.assertEqual(
+            wintersun.standardize_filename(filepath), 'markdown')
+
+    @mock.patch('wintersun.filenames_by_date')
+    def test_generate_post_index(self, mock_filenames_by_date):
+        mock_filenames_by_date.return_value = ('file1', 'file2',)
+        dir_path = './wintersun/posts'
+        files, path = wintersun.generate_post_index('posts.md', dir_path)
+        self.assertEqual(dir_path + '/posts', path)
+        self.assertIn('file1', files)
+        self.assertIn('file2', files)
+
+    @mock.patch('__builtin__.open')
+    @mock.patch('wintersun.MarkdownTransformer')
+    @mock.patch('wintersun.filter_items_from_path')
+    def test_filenames_by_date(self, mock_filter_items_from_path,
+                               mock_transformer, mock_open):
+
+        mock_filter_items_from_path.return_value = ('file1.md', 'file2.md', )
+
+        mock_transformer.return_value.__enter__.return_value = mock_transformer
+        type(mock_transformer).Meta = mock.PropertyMock(side_effect=[
+            {'title': ['title1'],
+                'date': ['2015-09-22 20:00:00']},
+            {'title': ['title2'],
+                'date': ['2015-08-22 20:00:00']}
+        ])
+
+        sorted_filenames = wintersun.filenames_by_date('./posts')
+        self.assertEqual(sorted_filenames[0].title, 'title1')
+        self.assertEqual(sorted_filenames[1].title, 'title2')
+
+    def test_generate_entry_link(self):
+        file_name = 'file1'
+        path = './wintersun/posts'
+        link = wintersun.generate_entry_link(file_name, path)
+        self.assertEqual(link,
+                         wintersun.SITE_URL + '/wintersun/posts/file1.html')
+
+    def test_generate_atom_entry_dict(self):
+        meta = {'title': ['title1'],
+                'date': ['2015-09-22']}
+        contents = 'Lorem ipsum ' * 25
+        file_name = 'file1'
+        path = './wintersun/posts'
+        expected_result = {
+            u'title': meta['title'][0],
+            u'link': wintersun.SITE_URL + '/wintersun/posts/file1.html',
+            u'published': '2015-09-22T00:00:00+01:00',
+            u'updated': '2015-09-22T00:00:00+01:00',
+            u'name': u'Matt',
+            u'content': contents[:100] + '...'}
+
+        entry = wintersun.generate_atom_entry_dict(contents, file_name, path,
+                                                   meta)
+
+        self.assertEqual(entry, expected_result)
+
+    @mock.patch('wintersun.generate_atom_entry_dict')
+    def test_update_feed(self, mock_entry_dict):
+        feed = mock.Mock()
+
+        wintersun.update_feed(feed, '', 'file1', 'wintersun/', {})
+        self.assertTrue(mock_entry_dict.called)
+        self.assertTrue(feed.add_entry.called)
+
+    @mock.patch('wintersun.os_path.join')
+    def test_build_path(self, mock_os_path):
+        wintersun.build_path('file1', './wintersun/posts')
+        self.assertTrue(mock_os_path.called)
+
+    @mock.patch('__builtin__.open')
+    def test_write_output_file(self, mock_open):
+        target_dir = wintersun.TARGET_DIR = 'target'
+        path = 'wintersun/posts'
+        filename = 'file1'
+
+        wintersun.write_output_file(path, filename, '')
+        mock_open.assert_called_with(target_dir + '/' + path + '/' +
+                                     'file1.html', mode='w')
+        self.assertTrue(mock_open().__enter__().write.called)
+
+    @mock.patch('wintersun.env')
+    def test_apply_output_template_post(self, mock_env):
+        mock_template = mock.Mock()
+        mock_env.get_template.return_value = mock_template
+
+        file_name = 'file1.md'
+        path = 'wintersun/posts'
+        meta = {'title': ['title'], 'template': ['Post']}
+        contents = 'Lorem ipsum ' * 100
+
+        wintersun.apply_output_template(file_name, path, contents, meta)
+        self.assertTrue(mock_env.get_template.called)
+        self.assertTrue(mock_template.render.called)
+
+    @mock.patch('wintersun.env')
+    @mock.patch('wintersun.generate_post_index')
+    def test_apply_output_template_index(self, mock_gen_post_index, mock_env):
+        mock_gen_post_index.return_value = (1, 2,)
+        file_name = 'file1.md'
+        path = 'wintersun/posts'
+        meta = {'title': ['title'], 'template': ['Index']}
+        contents = 'Lorem ipsum ' * 100
+
+        wintersun.apply_output_template(file_name, path, contents, meta)
+        self.assertTrue(mock_gen_post_index.called)
+
+    @mock.patch('wintersun.build_tree')
+    def test_transform_next_dir_level_no_directories(self, mock_build_tree):
+        path = 'wintersun/posts'
+        directories = []
+
+        wintersun.transform_next_dir_level(path, directories)
+
+        self.assertFalse(mock_build_tree.called)
+
+    @mock.patch('wintersun.build_tree')
+    def test_transform_next_dir_level_excluded_dirs(self, mock_build_tree):
+        path = 'wintersun/posts'
+
+        wintersun.transform_next_dir_level(path, wintersun.EXCLUDED_DIRS)
+        self.assertFalse(mock_build_tree.called)
+
+    @mock.patch('wintersun.os.mkdir')
+    @mock.patch('wintersun.build_tree')
+    def test_transform_next_dir_level(self, mock_build_tree, mock_mkdir):
+        path = 'wintersun/posts'
+        directories = ['dir1', 'dir2']
+        expected_path = wintersun.TARGET_DIR = '/' + path
+
+        wintersun.transform_next_dir_level(path, directories)
+
+        mock_mkdir.assert_has_call([mock.call(expected_path + '/dir1'),
+                                    mock.call(expected_path + '/dir2')])
+        self.assertTrue(mock_build_tree.called)
+
+    @mock.patch('__builtin__.open')
+    @mock.patch('wintersun.transform_next_dir_level')
+    @mock.patch('wintersun.filter_items_from_path')
+    @mock.patch('wintersun.MarkdownTransformer')
+    @mock.patch('wintersun.apply_output_template')
+    @mock.patch('wintersun.write_output_file')
+    def test_build_tree(self, mock_write_output_file,
+                        mock_apply_output_template, mock_md_tranformer,
+                        mock_filter_items_from_path,
+                        mock_transform_next_dir_level, mock_open):
+        mock_filter_items_from_path.side_effect = [[], ['file1.md', 'file2.md']
+                                                   ]
+
+        wintersun.build_tree('wintersun/posts')
+
+        self.assertEqual(mock_apply_output_template.call_count, 2)
+        self.assertEqual(mock_write_output_file.call_count, 2)
+        self.assertTrue(mock_transform_next_dir_level.called)
+
+    @mock.patch('wintersun.copytree')
+    @mock.patch('wintersun.os.mkdir')
+    @mock.patch('wintersun.os_path')
+    @mock.patch('wintersun.rmtree')
+    @mock.patch('wintersun.ARGS')
+    def test_prepare_target_dir_target_dir_exists(self, mock_ARGS, mock_rmtree,
+                                                  mock_os_path, mock_mkdir,
+                                                  mock_copytree):
+        mock_os_path.exists.return_value = True
+        mock_os_path.isdir.return_value = True
+        mock_ARGS.delete = mock.PropertyMock(return_value=True)
+
+        wintersun.prepare_target_dir()
+
+        self.assertTrue(mock_rmtree.called)
+        self.assertTrue(mock_mkdir.called)
+        self.assertTrue(mock_copytree.called)
+
+    @mock.patch('wintersun.copytree')
+    @mock.patch('wintersun.os.mkdir')
+    @mock.patch('wintersun.os_path')
+    def test_prepare_target_dir_target_dir_dont_exist(self,
+                                                      mock_os_path,
+                                                      mock_mkdir,
+                                                      mock_copytree):
+        mock_os_path.exists.return_value = False
+
+        wintersun.prepare_target_dir()
+
+        self.assertTrue(mock_mkdir.called)
+        self.assertTrue(mock_copytree.called)
+
+
+class TestMarkdownTransformer(unittest.TestCase):
+
+    @mock.patch('wintersun.markdown.Markdown')
+    def test_convert_utf8(self, mock_md):
+        flike = mock.Mock()
+        flike.read.return_value = 'test\n'
+        md_instance_mock = mock.Mock()
+        mock_md.return_value = md_instance_mock
+        md = wintersun.MarkdownTransformer()
+
+        md.convert_utf8(flike)
+
+        self.assertTrue(flike.read.called)
+        self.assertTrue(md_instance_mock.convert.called)
+
+    @mock.patch('wintersun.markdown.Markdown')
+    def test_md_transformer_reset(self, mock_md):
+        with wintersun.MarkdownTransformer() as md:
+            md.Meta
+
+        self.assertTrue(mock_md.called)
+        self.assertTrue(mock_md().reset.called)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- wintersun.py
+++ wintersun.py
@@ -12,15 +12,15 @@ from datetime import datetime
 
 from atom_generator import Feed, create_timestamp
 
-# make these live in a global dictionary
 ARGS = None
+
 MARKDOWN_FILTER = re.compile(r'([a-zA-Z0-9_-]+)\.md')
 TEMPLATED_FILENAME_FILTER = re.compile(r'[^a-z^A-Z^0-9-]')
-REMOVE_LEADING_SLASHES = re.compile(r'^[a-zA-Z0-9\.\/]*\/')
-REMOVE_RELATIVE_SLASHES = re.compile(r'\./')
-REMOVE_TRAILING_SUFFIX = re.compile(r'\.md$')
-SITE_URL = 'http://mattscodecave.com'
+LEADING_SLASHES = re.compile(r'^[a-zA-Z0-9\.\/]*\/')
+RELATIVE_SLASHES = re.compile(r'\./')
+TRAILING_SUFFIX = re.compile(r'\.md$')
 
+SITE_URL = 'http://mattscodecave.com'
 TEMPLATE_DIR = './wintersun/templates'
 STATIC_DIR = './wintersun/static'
 TARGET_DIR = './site'
@@ -47,77 +47,71 @@ FEED = Feed([
 env = Environment(loader=PackageLoader('wintersun'))
 
 
-def get_items_from_path(path, fn=os_path.isfile):
-    for filename in os.listdir(path):
-        full_path = os_path.join(path, filename)
-        if fn(full_path):
-            yield full_path
+class MarkdownTransformer(object):
+    def __init__(self):
+        self.md = markdown.Markdown(extensions=['markdown.extensions.meta'])
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.md.reset()
+        return False
+
+    def convert(self, *args, **kwargs):
+        return self.md.convert(*args, **kwargs)
+
+    def convert_utf8(self, f):
+        return self.convert(unicode(f.read(), 'utf-8'))
+
+    @property
+    def Meta(self):
+        return self.md.Meta
 
 
-def get_markdown_files(filenames):
-    for filename in filenames:
-        if MARKDOWN_FILTER.search(filename):
-            yield filename
-
-
-def standardize_filename(filename):
-    return TEMPLATED_FILENAME_FILTER.sub(
-        '-',
-        REMOVE_TRAILING_SUFFIX.sub(
-            '',
-            REMOVE_LEADING_SLASHES.sub('', filename)))
-
-
-def build_tags(path):
-    # process files looking at Meta for tags
-    # create list of namedtuples with title: tag
-    # use this to create index of titles for each tag
-    pass
-
-
-def convert_date(date_string):
-    return datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
-
-
-def filenames_by_date(dir_path):
-    PostItem = namedtuple('PostItem', 'filename, title, date')
-    files = []
+def build_tree(path):
+    # pull out loop?
+    # responsiblities here: get files in current folder, get subfolders,
+    # generate markup, apply template, write template, update feed
+    # calling lots of othe functions on multiple levels :/
+    directories = filter_items_from_path(path, filter_fn=os_path.isdir)
     md_files = get_markdown_files(
-        get_items_from_path(dir_path, fn=os_path.isfile))
-    md = markdown.Markdown(extensions=['markdown.extensions.meta'])
+        filter_items_from_path(path, filter_fn=os_path.isfile))
 
     for md_file in md_files:
-        with open(md_file, 'rb') as f:
-            md.convert(unicode(f.read(), 'utf-8'))
-            files.append(
-                PostItem._make((standardize_filename(md_file),
-                                md.Meta['title'][0],
-                                convert_date(md.Meta['date'][0]),)))
-            md.reset()
+        print u'md_file: {}'.format(md_file)
+        with MarkdownTransformer() as md:
+            with open(md_file) as f:
+                marked_up = md.convert_utf8(f)
+                templated_item = apply_output_template(md_file, path,
+                                                       marked_up, md.Meta)
+                if u'Post' in md.Meta['template']:
+                    update_feed(FEED, marked_up, md_file, path, md.Meta)
 
-    return sorted(
-        files,
-        key=lambda file_tuple: file_tuple.date,
-        reverse=True), dir_path
+                write_output_file(path, md_file, templated_item)
 
-
-def build_index(path, filename):
-    directory_name = MARKDOWN_FILTER.search(filename).groups()
-
-    if not directory_name:
-        return []
-
-    directory_name = directory_name[0]
-    directory_path = os_path.join(path, directory_name)
-    return filenames_by_date(directory_path)
+    transform_next_dir_level(path, directories)
 
 
-def templated_content(filename, path, contents, meta):
-    template = env.get_template(meta['template'][0].lower() + '.html')
-    # debugging
+def transform_next_dir_level(path, directories):
+    # abstraction level mix - too many topics?
+    for directory in directories:
+        if directory not in EXCLUDED_DIRS:
+            print u'making dir: {}'.format(directory)
+            os.mkdir(os_path.join(TARGET_DIR, path, directory), 0755)
+            build_tree(os_path.join(path, directory))
+
+
+def apply_output_template(md_file, path, contents, meta):
+    # replace meta with template name - get template via function
+    # replace meta with title
+    # replace conditional with function
+    # rename vars ie contents, or meta
     print u'template meta title: {}'.format(meta['title'][0])
-    if meta['template'][0] in ('Index', 'Main',):
-        meta['indexed'], meta['indexed_dir'] = build_index(path, filename)
+    template = env.get_template(meta['template'][0].lower() + '.html')
+    if meta['template'][0] in ('Index', 'Main', ):
+        meta['indexed'], meta['indexed_dir'] = generate_post_index(md_file,
+                                                                   path)
 
     return template.render(
         path=path,
@@ -125,14 +119,67 @@ def templated_content(filename, path, contents, meta):
         meta=meta)
 
 
+def write_output_file(path, file_name, contents):
+    print u'creating file: {}'.format(
+        standardize_filename(file_name.strip()) + '.html')
+
+    output_destination_path = os_path.join(
+        TARGET_DIR,
+        path,
+        standardize_filename(file_name) + '.html')
+
+    with open(output_destination_path, mode='w') as out:
+        # encoding is a separate responsiblity
+        # this fn should just write to the right spot
+        out.write(contents.encode('utf-8-sig'))
+
+
+def generate_post_index(post_file, path):
+    index_directory_name = MARKDOWN_FILTER.search(post_file).groups()
+
+    if not index_directory_name:
+        return []
+
+    index_directory_path = os_path.join(path, index_directory_name[0])
+    return filenames_by_date(index_directory_path), index_directory_path
+
+
+PostItem = namedtuple('PostItem', 'filename, title, date')
+
+
+def create_postitem(file_name, file_meta):
+    return PostItem._make((
+        standardize_filename(file_name),
+        file_meta['title'][0],
+        convert_date_time(file_meta['date'][0]),))
+
+
+def filenames_by_date(dir_path):
+    files = []
+    input_files = get_markdown_files(
+        filter_items_from_path(dir_path, filter_fn=os_path.isfile))
+
+    with MarkdownTransformer() as md:
+        for input_file in input_files:
+            with open(input_file, 'rb') as f:
+                md.convert_utf8(f)
+                input_file_meta = md.Meta
+                files.append(create_postitem(input_file, input_file_meta))
+
+    return sorted(
+        files,
+        key=lambda post_item: post_item.date,
+        reverse=True)
+
+
 def generate_entry_link(md_file, path):
     return '/'.join(
         [SITE_URL,
-         REMOVE_RELATIVE_SLASHES.sub('', path),
+         RELATIVE_SLASHES.sub('', path),
          standardize_filename(md_file) + '.html'])
 
 
-def generate_atom_entry(contents, md_file, path, meta):
+def generate_atom_entry_dict(contents, md_file, path, meta):
     entry = {
         u'title': meta['title'][0],
         u'link': generate_entry_link(md_file, path),
@@ -144,46 +191,48 @@ def generate_atom_entry(contents, md_file, path, meta):
     return entry
 
 
-def write_templated_file(path, output_file, contents):
-    print u'creating file: {}'.format(
-        standardize_filename(output_file.strip()) + '.html')
-
-    output = os_path.join(
-        TARGET_DIR,
-        path,
-        standardize_filename(output_file) + '.html')
-
-    with open(output, mode='w') as out:
-        out.write(contents.encode('utf-8-sig'))
+def update_feed(feed, marked_up, md_file, path, md_meta):
+    feed.add_entry(generate_atom_entry_dict(marked_up, md_file, path, md_meta))
 
 
-def build_tree(path):
-    directories = get_items_from_path(path, fn=os_path.isdir)
-    md_files = get_markdown_files(get_items_from_path(path, fn=os_path.isfile))
+def build_path(node, path):
+    return os_path.join(path, node)
 
-    md = markdown.Markdown(extensions=['markdown.extensions.meta'])
 
-    for md_file in md_files:
-        print u'md_file: {}'.format(md_file)
-        with open(md_file) as f:
-            marked_up = md.convert(unicode(f.read(), 'utf-8'))
-            templated_item = templated_content(md_file, path,
-                                               marked_up, md.Meta)
-            if u'Post' in md.Meta['template']:
-                FEED.add_entry(generate_atom_entry(
-                    marked_up, md_file, path, md.Meta))
-            md.reset()
+def filter_items_from_path(path, filter_fn=os_path.isfile):
+    for item in os.listdir(path):
+        full_item_path = build_path(item, path)
+        if filter_fn(full_item_path):
+            yield full_item_path
 
-            write_templated_file(path, md_file, templated_item)
 
-    for directory in directories:
-        if directory not in EXCLUDED_DIRS:
-            print u'making dir: {}'.format(directory)
-            os.mkdir(os_path.join(TARGET_DIR, path, directory), 0755)
-            build_tree(os_path.join(path, directory))
+def get_markdown_files(filenames):
+    for filename in filenames:
+        if MARKDOWN_FILTER.search(filename):
+            yield filename
+
+
+def standardize_filename(filename):
+    return TEMPLATED_FILENAME_FILTER.sub(
+        '-',
+        TRAILING_SUFFIX.sub(
+            '',
+            LEADING_SLASHES.sub('', filename)))
+
+
+def build_tags(path):
+    # process files looking at Meta for tags
+    # create list of namedtuples with title: tag
+    # use this to create index of titles for each tag
+    pass
+
+
+def convert_date_time(date_string):
+    return datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
 
 
 def prepare_target_dir():
+    # extract contents of if
     def setup_target_dir():
         os.mkdir(TARGET_DIR, 0755)
         copytree(STATIC_DIR, os_path.join(TARGET_DIR, './static'))
@@ -209,6 +258,10 @@ def set_settings(args):
 
 
 if __name__ == '__main__':
+    # add logger here
+    # align functions top to bottom
+    # analyze in term of abstraction level mixing
+    # pull out into other modules
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--delete',
                         help=("Remove target output directory before "
